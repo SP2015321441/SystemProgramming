b25ee831f227ab201d76b7dd2f4e13edc42bd622
youngwookim <drywkim@gmail.com>
2016-03-23 14:32:32 +0900

    20160323 homework

---
+++ young3.c
@@ -0,0 +1,19 @@
+#include <grp.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+char *gid_to_name (gid_t gid)
+
+{
+struct group *getgrgid(), *grp_ptr;
+static char numstr[10];
+
+if (( grp_ptr = getgrgid(gid))==NULL){
+     sprintf(numstr,"%d", gid);
+     return numstr;
+}
+else
+     return grp_ptr->gr_name;
+}
+
